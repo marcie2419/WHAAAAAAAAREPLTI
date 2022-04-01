20b2a91b960ab25d34bc6b9a435c8a86442064a7
marcie2419 <78964935+marcie2419@users.noreply.github.com>
2022-04-01 16:34:25 +0200

    Revert "🖌 interaction rank card"
    This reverts commit 72b0a3b1c177302f4ed6732f62899c52a9b309b0.

--- src/blocks/discord/discord-canvas/goodbye/send_in_channel.js
+++ src/blocks/discord/discord-canvas/goodbye/send_in_channel.js
@@ -4,7 +4,7 @@ import "@blockly/field-grid-dropdown";
 const blockName = "s4d_send_in_channel";
 
 const blockData = {
-    "message0": "send %1 in channel %2  as a %3 reply with goodbye/welcome/rankcard %4",
+    "message0": "%{BKY_SEND_IN_CHANNEL}",
     "args0": [
       {
         "type":"input_value",
@@ -15,20 +15,6 @@ const blockData = {
         "type":"input_value",
         "name":"CHANNEL",
         "check":"Channel"
-      },
-      {
-        "type": "field_grid_dropdown",
-        "name": "INTERACTION",
-        "options": [
-          [
-            "message",
-            "message"
-          ],
-          [
-            "interaction",
-            "interaction"
-          ]
-        ]
       },
 			{
         "type":"input_value",
@@ -54,14 +40,6 @@ Blockly.JavaScript[blockName] = function(block){
     const channel = Blockly.JavaScript.valueToCode(block, "CHANNEL", Blockly.JavaScript.ORDER_ATOMIC);
 		const content = Blockly.JavaScript.valueToCode(block, "CONTENT", Blockly.JavaScript.ORDER_ATOMIC);
 		const gw = Blockly.JavaScript.valueToCode(block, "GW", Blockly.JavaScript.ORDER_ATOMIC);
-    const info2 = block.getFieldValue("INTERACTION");
-    let info1 = info2.replace("'", '')
-    let info = info1.replace("'", "")
-    let code;
-    if(info === 'interaction') {
-      code = `interaction.reply({content:${content},files:[{attachment:${gw}.toBuffer()}]});\n`
-    } else {
-      code = `${channel}.send({content:${content},files:[{attachment:${gw}.toBuffer()}]});\n`;
-    }
-    return code
+let code = `${channel}.send({content:${content},files:[{attachment:${gw}.toBuffer()}]});\n`;
+return code
 };
